5e995e8bf8a94fc65ff2f09813ca90276a221266
crumwj22 <crumwj22@hotmail.com>
2022-05-07 23:18:19 -0700

    updated server.js with all request code

--- server.js
+++ server.js
@@ -1,91 +1,73 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
 
-const uuid = require("./helpers/uuid");
-
-const PORT = 3001;
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(express.static("public"));
-
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
-
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
-);
+//static middleware
+app.use(express.static("./develope/public"));
 
 // GET request for notes
 app.get("/api/notes", (req, res) => {
-  res.json(database);
+  readFile("./develop/db/db.json", "utf8", (err, data) => {
+    notes = [].concat(JSON.parse(data));
+    res.json(notes);
+  });
 });
 
 // POST request to add a note
 app.post("/api/notes", (req, res) => {
-  
-  let newNote = req.body;
-  notes.push(newNote);
-  updateDb();
-  return console.log("added new note: "+newNote.title)
+  const newNote = req.body;
+  readFile("./develop/db/db.json", "utf8", (err, data) => {
+    const notes = [].concat(JSON.parse(data));
+    newNote.id = notes.length + 1;
+    notes.push(newNote);
+    return notes;
+  }).then(function (notes) {
+    writeFile("./develop/db/db.json", JSON.stringify(notes));
+    res.json(newNote);
+  });
 });
-
-app.get("/api/notes/:id",  (req, res) => {
-res.json(notes[req.params.id])
-});
-
-    // Obtain existing notes
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        // Convert string into JSON object
-        const notes = JSON.parse(data);
-
-        // Add a new note
-        notes.push(newNote);
-
-        // Write updated reviews back to the file
-   
+// delete request
+app.delete("/api/notes/:id", (req, res) => {
+  const deleteId = parseInt(req.params.id);
+  readFile("./develop/db/db.json", "utf8", (err, data) => {
+    const notes = [].concat(JSON.parse(data));
+    const newNoteData = [];
+    for (let i = 0; i < notes.length; i++) {
+      if (deleteId !== notes[i].id) {
+        newNoteData.push(notes[i]);
       }
-    });
-
-function updateDb()
-     fs.writeFile(
-          "./db/db.json",
-          JSON.stringify(notes, null, 4),
-          (writeErr) =>
-            writeErr
-              ? console.error(writeErr)
-              : console.info("Successfully updated notes!")
-        );
-
-    
-function deleteNote(id, notesArray) {
-    for (let i = 0; i < notesArray.length; i++) {
-        let note = notesArray[i];
-
-        if (note.id == id) {
-            notesArray.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, `./db/db.json`),
-                JSON.stringify(notesArray, null, 2)
-            );
-            break;
-        }
     }
-}
-
-app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, allNotes);
-    res.json(true);
+    return newNoteData;
+  }).then(function (notes) {
+    writeFile("./develop/db/db.json", JSON.stringify(notes));
+    res.send("saved successfully!");
+  });
 });
 
+// HTML routes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+});
+
+// listening
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
