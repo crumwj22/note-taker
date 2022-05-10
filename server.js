8c044111c816e6903e23b9d3f7661f6fd84320c0
crumwj22 <crumwj22@hotmail.com>
2022-05-09 22:07:53 -0700

    fis heroku connection

--- server.js
+++ server.js
@@ -12,21 +12,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//static middleware
-app.use(express.static("./develop/public"));
-
-// HTML routes
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./develop/public/index.html"));
-});
-
 // GET request for notes, display note
 app.get("/api/notes", (req, res) => {
-  fs.readFile("./develop/db/db.json", "utf8", (err, data) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
     notes = JSON.parse(data);
     console.log(notes);
     res.json(notes);
@@ -35,14 +23,14 @@ app.get("/api/notes", (req, res) => {
 
 // POST request to add a note, new note
 app.post("/api/notes", (req, res) => {
-  fs.readFile("./develop/db/db.json", "utf8", (err, data) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
     const newNote = req.body;
     const notes = JSON.parse(data);
     newNote.id = uuid.v4();
     notes.push(newNote);
 
     // fs.writeFile("./develop/db/db.json", (err, data) => JSON.stringify(notes));
-    fs.writeFile("./develop/db/db.json", JSON.stringify(notes), (err) => {
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
       if (err) {
         console.error(err);
       }
@@ -55,13 +43,13 @@ app.post("/api/notes", (req, res) => {
 // DELETE request, delete saved note
 app.delete("/api/notes/:id", (req, res) => {
   const deleteId = req.params.id;
-  fs.readFile("./develop/db/db.json", "utf8", (err, data) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
     const notes = JSON.parse(data);
     const newNoteData = notes.filter((item) => {
       return item.id !== deleteId;
     });
 
-    fs.writeFile("./develop/db/db.json", JSON.stringify(newNoteData), (err) => {
+    fs.writeFile("./db/db.json", JSON.stringify(newNoteData), (err) => {
       if (err) {
         console.error(err);
       }
@@ -71,8 +59,20 @@ app.delete("/api/notes/:id", (req, res) => {
   });
 });
 
+//static middleware
+app.use(express.static("public"));
+
+// HTML routes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 // listening
