8c560d594756b2c186dd9fd5bffafcd9097f641f
crumwj22 <crumwj22@hotmail.com>
2022-05-09 22:44:00 -0700

    deleted a line that was not needed

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-// const util = require("util");
 
 // method for generating unique ids
 const uuid = require("uuid");
