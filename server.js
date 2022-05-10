37f673c1fcf527ff1bd560227a937cb0f63529a3
crumwj22 <crumwj22@hotmail.com>
2022-05-09 18:20:46 -0700

    updated server.js with new fs.write data

--- server.js
+++ server.js
@@ -13,42 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //static middleware
-app.use(express.static("./develope/public"));
-
-// GET request for notes
-app.get("/api/notes", (req, res) => {
-  fs.readFile("./develop/db/db.json", "utf8", (err, data) => {
-    notes = JSON.parse(data);
-    res.json(notes);
-  });
-});
-
-// POST request to add a note
-app.post("/api/notes", (req, res) => {
-  fs.readFile("./develop/db/db.json", "utf8", (err, data) => {
-    const newNote = req.body;
-    const notes = JSON.parse(data);
-    newNote.id = uuid.v4();
-    notes.push(newNote);
-
-    fs.writeFile("./develop/db/db.json", JSON.stringify(notes));
-    res.json(newNote);
-  });
-});
-
-// delete request
-app.delete("/api/notes/:id", (req, res) => {
-  const deleteId = req.params.id;
-  fs.readFile("./develop/db/db.json", "utf8", (err, data) => {
-    const notes = JSON.parse(data);
-    const newNoteData = notes.filter((item) => {
-      return item.id !== deleteId;
-    });
-
-    fs.writeFile("./develop/db/db.json", JSON.stringify(notes));
-    res.json(newNoteData);
-  });
-});
+app.use(express.static("./develop/public"));
 
 // HTML routes
 app.get("/notes", (req, res) => {
@@ -57,6 +22,53 @@ app.get("/notes", (req, res) => {
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+});
+
+// GET request for notes, display note
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./develop/db/db.json", "utf8", (err, data) => {
+    notes = JSON.parse(data);
+    console.log(notes);
+    res.json(notes);
+  });
+});
+
+// POST request to add a note, new note
+app.post("/api/notes", (req, res) => {
+  fs.readFile("./develop/db/db.json", "utf8", (err, data) => {
+    const newNote = req.body;
+    const notes = JSON.parse(data);
+    newNote.id = uuid.v4();
+    notes.push(newNote);
+
+    // fs.writeFile("./develop/db/db.json", (err, data) => JSON.stringify(notes));
+    fs.writeFile("./develop/db/db.json", JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+      }
+      // file written successfully
+    });
+    res.json(newNote);
+  });
+});
+
+// DELETE request, delete saved note
+app.delete("/api/notes/:id", (req, res) => {
+  const deleteId = req.params.id;
+  fs.readFile("./develop/db/db.json", "utf8", (err, data) => {
+    const notes = JSON.parse(data);
+    const newNoteData = notes.filter((item) => {
+      return item.id !== deleteId;
+    });
+
+    fs.writeFile("./develop/db/db.json", JSON.stringify(newNoteData), (err) => {
+      if (err) {
+        console.error(err);
+      }
+      // file written successfully
+    });
+    res.json(newNoteData);
+  });
 });
 
 app.get("*", (req, res) => {
